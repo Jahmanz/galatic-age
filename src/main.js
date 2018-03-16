6119148d0b841438dcbde5a6270fe22022a38044
Jahmanz Williams <savojah@yahoo.com>
2018-03-16 14:24:57 -0700

    inital project setup

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import { Date } from './galatic-age.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function(){
+  $('#date').submit(function(event){
+    event.preventDefault();
+    $('#dateShow').text();
+
+    let birthdate = $('#birthdate').val();
+
+
+    $('#birthdateShow').text(birthdate);
+    let userDate = new Date(birthdate);
+
+    let userDateEarth = userDate.ageInYears();
+    $('#ageEarth').text(userDateEarth);
+    let userDateMercury = userDate.ageOnMercury();
+    $('#ageMercury').text(userDateMercury);
+    let userDateVenus = userDate.ageOnVenus();
+    $('#ageVenus').text(userDateVenus);
+    let userDateMars = userDate.ageOnMars();
+    $('#ageMars').text(userDateMars);
+    let userDateJupiter = userDate.ageOnJupiter();
+    $('#ageJupiter').text(userDateJupiter);
+
+
+    // let userTimeEarth = userDate.yearsLeftEarth();
+    // $('#timeLeftEarth').text(userTimeEarth);
+    // let userTimeMercury = userDate.yearsLeftMercury();
+    // $('#timeLeftMercury').text(userTimeMercury);
+    // let userTimeVenus = userDate.yearsLeftVenus();
+    // $('#timeLeftVenus').text(userTimeVenus);
+    // let userTimeMars = userDate.yearsLeftMars();
+    // $('#timeLeftMars').text(userTimeMars);
+    // let userTimeJupiter = userDate.yearsLeftJupiter();
+    // $('#timeLeftJupiter').text(userTimeJupiter);
+  });
+});
